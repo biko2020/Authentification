b42219069d6e1136c5e301c378512ed6cebb95d2
biko2020 <aitoufkirbrahimab@gmail.com>
2023-08-09 17:11:31 +0100

    :lipstick: add UI inscription with style scss

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+// GET home page.
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Authentification using node.js' });
 });
 
+
+// Get route_to_Inscription interface
+router.get("/routeToInscription",
+  function (req, res, next) {
+    res.render("inscription/index", {
+      title: "Inscrivez-vous"
+    })
+  });
 
 module.exports = router;
