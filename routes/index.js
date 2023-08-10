f263865edaa591420ac1013030e5b53813ed81ed
biko2020 <aitoufkirbrahimab@gmail.com>
2023-08-10 17:53:51 +0100

    :sparkles: intoduce a registation form

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,21 @@ router.get('/', function (req, res, next) {
 });
 
 
-// Get route_to_Inscription interface
+// Get route_to_Inscription ,interface d'inscription
 router.get("/routeToInscription",
   function (req, res, next) {
     res.render("inscription/index", {
       title: "Inscrivez-vous"
-    })
+    });
   });
+
+
+// Get route_To_Registration_By_Email  ,formulaire inscription par e-mail
+router.get("/routeToRegistrationByEmail", function (req, res, next) {
+  res.render("inscription/index", {
+    title: "Inscription par e-mail",
+    registration_type: "email"
+  });
+});
 
 module.exports = router;
